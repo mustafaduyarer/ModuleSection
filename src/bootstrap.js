14222f3951e5c159e4d0aa5bafa0355ab8e0411a
mustafaduyarer <mustafaduyarer@gmail.com>
2024-05-17 17:14:29 +0200

    first commit

---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+// ES6 code goes here
+//console.log('es6 code goes here');
+
+import { greeting } from "./helper";
+
+console.log(greeting)
+
+
+import { multiply } from './helper';
+
+console.log(multiply(2, 5));
---
+++ src/helper.js
@@ -0,0 +1,5 @@
+export const greeting = 'Hi there mustafa'
+
+export function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+  }
